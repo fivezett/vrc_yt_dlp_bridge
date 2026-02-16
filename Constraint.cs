a1794de7036477c93e86464e310595918a27476c
fivezett <108619066+fivezett@users.noreply.github.com>
2026-02-17 02:20:19 +0900

    refactor: Constraintに追加したほうが良いものを追加

--- Constraint.cs
+++ Constraint.cs
@@ -8,4 +8,9 @@
     public static readonly string TmpDirectory = Path.Combine(YtDlpWorkingDirPath, "tmp");
     public static readonly string HomeDirectory = Path.Combine(YtDlpWorkingDirPath, "home");
     public static readonly string YtDlpClientUpdateDatePath = Path.Combine(YtDlpWorkingDirPath, "yt-dlp_last_update.txt");
+    public const string DenoVersionUrl = "https://dl.deno.land/release-latest.txt";
+    public const string DenoDownloadUrlTemplate = "https://dl.deno.land/release/{0}/deno-x86_64-pc-windows-msvc.zip";
+    public const int YtDlpUpdateIntervalDays = 1;
+    public const int LogSeparatorLength = 80;
 }
+    
